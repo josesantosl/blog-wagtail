2bcf6afec1791b5d7df3e4ebfb6e916e529602d8
Jose Santos L <josesantosl1607@gmail.com>
2022-08-30 15:54:30 +0200

    comenzamos

---
+++ test1/test1/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xfl-g@g1m3)w2qzafg@vsp+hpe(-r(uit-uch#e7#=i6ggc8ve"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
