e7eb253464f7cb30263e62f2d66f60e50f1c7b23
Jose Santos L <josesantosl1607@gmail.com>
2022-09-05 13:33:33 +0200

    add column title

--- test2.py
+++ test2.py
@@ -12,17 +12,23 @@ def main():
     worksheet= workbook.add_worksheet()
 
 
+    #set tittle column
+    worksheet.write(0,0,"titolo")
+    worksheet.write(0,1,"descrizione")
+    worksheet.write(0,2,"testo")
+    worksheet.write(0,3,"data")
+    worksheet.write(0,4,"autore")
 
     for row,item in enumerate(listArticoli['items']):
         print(item['title'])
         url= requests.get('http://localhost:8000/api/v2/pages/'+str(item['id'])+"/?format=json")
         print(url.text)
         pagina = json.loads(url.text)
-        worksheet.write(row,0,pagina['title'])
-        worksheet.write(row,1,pagina['descrizione'])
-        worksheet.write(row,2,pagina['testo'])
-        worksheet.write(row,3,pagina['data'])
-        worksheet.write(row,4,pagina['autore'])
+        worksheet.write(row+1,0,pagina['title'])
+        worksheet.write(row+1,1,pagina['descrizione'])
+        worksheet.write(row+1,2,pagina['testo'])
+        worksheet.write(row+1,3,pagina['data'])
+        worksheet.write(row+1,4,pagina['autore'])
 
     workbook.close()
 
